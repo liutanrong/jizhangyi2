2d8d50b529dadd0f0e907fc46934aa9f16bf27cb
牧笙 <tanrong.ltr@alibaba-inc.com>
2016-07-07 21:41:56 +0800

    网络故障时自动重发

---
+++ app/src/main/java/com/liu/Account/receiver/NetworkBroadcastReceiver.java
@@ -0,0 +1,7 @@
+package com.liu.Account.receiver;
+
+/**
+ * Created by tanrong on 16/7/7.
+ */
+public class NetworkBroadcastReceiver {
+}
---
+++ app/src/main/java/com/liu/Account/service/RepeatNetworkService.java
@@ -0,0 +1,7 @@
+package com.liu.Account.service;
+
+/**
+ * Created by tanrong on 16/7/7.
+ */
+public class RepeatNetworkService {
+}
