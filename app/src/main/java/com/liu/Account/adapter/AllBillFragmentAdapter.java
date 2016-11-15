f67a34a05c2d3baa4495fded835ed9a6451e97de
liutanrong <liutanrong0425@163.com>
2016-11-15 11:08:13 +0800

    stash

---
+++ app/src/main/java/com/liu/Account/adapter/AllBillFragmentAdapter.java
@@ -0,0 +1,8 @@
+package com.liu.Account.adapter;
+
+/**
+ * Created by tanrong on 16/10/17.
+ */
+
+public class AllBillFragmentAdapter {
+}
---
+++ app/src/main/java/com/liu/Account/model/AllBillFragmentData.java
@@ -0,0 +1,35 @@
+package com.liu.Account.model;
+
+/**
+ * Created by tanrong on 16/10/17.
+ */
+
+public class AllBillFragmentData {
+    private String dateTime;
+    private String totalMoney;
+    private Integer moneyType; //1是收入 2是支出
+
+    public String getDateTime() {
+        return dateTime;
+    }
+
+    public void setDateTime(String dateTime) {
+        this.dateTime = dateTime;
+    }
+
+    public String getTotalMoney() {
+        return totalMoney;
+    }
+
+    public void setTotalMoney(String totalMoney) {
+        this.totalMoney = totalMoney;
+    }
+
+    public Integer getMoneyType() {
+        return moneyType;
+    }
+
+    public void setMoneyType(Integer moneyType) {
+        this.moneyType = moneyType;
+    }
+}
